b4a0a713ab6be9825034fc43b5e1a1977cad82ca
fraziermatthew <fraziermatthew@hotmail.com>
2023-10-10 12:50:56 -0400

    feat: add params to config

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,12 +23,12 @@ vStore = Chroma(persist_directory=persist_directory, embedding_function=embeddin
 excl_vs = Chroma(persist_directory=excl_persist_directory, embedding_function=embeddings)
 
 _template = """You are a tutor for a high school student Computer Science Principles course.
-            Given the following documents and a question, create an answer the question.
-            If you don't know the answer, just say that you don't know, don't try to make up an answer.
-            All answers must be understandable for a high school student.
-            All answers should be succinct.
-            Encourage the learner to reflect on their personal experience by using follow-up questions.
-            Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+Given the following documents and a question, create an answer the question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+All answers must be understandable for a high school student.
+All answers should be succinct.
+Encourage the learner to reflect on their personal experience by using follow-up questions.
+Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
 
 Chat History:
 {chat_history}
@@ -37,7 +37,7 @@ Standalone question:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
 search_kwargs = {"k": 1}
-chatgpt = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
+chatgpt = ChatOpenAI(openai_api_key=st.secrets["openai_api_key"], temperature=0, model="gpt-3.5-turbo-16k")
 memory = ConversationSummaryMemory(llm=chatgpt,memory_key="chat_history", return_messages=True)
 retriever = vs.as_retriever(search_kwargs=search_kwargs)
 model = ConversationalRetrievalChain.from_llm(llm=chatgpt,
@@ -47,7 +47,8 @@ model = ConversationalRetrievalChain.from_llm(llm=chatgpt,
                                              condense_question_prompt=CONDENSE_QUESTION_PROMPT
                                              )
 
-excl_prompt_template = """Use the following pieces of context to reformulate the text. Reformulate your answer to a less technical understanding for a high school student if it should have been excluded.
+excl_prompt_template = """Use the following pieces of context to reformulate the text.
+Reformulate your answer to a less technical understanding for a high school student if it should have been excluded.
 
 {context}
 
@@ -56,7 +57,7 @@ Simplified Text:"""
 _excl_template = PromptTemplate(template=excl_prompt_template, input_variables=["context", "question"])
 chain_type_kwargs = {"prompt": _excl_template}
 ex_search_kwargs = {"k": 1,"score_threshold": 0.85}
-excl_chatgpt=OpenAI(temperature=0, model="gpt-3.5-turbo-instruct")
+excl_chatgpt=OpenAI(openai_api_key=st.secrets["openai_api_key"], temperature=0, model="gpt-3.5-turbo-instruct")
 excl_retriever=excl_vs.as_retriever(search_type="similarity_score_threshold", search_kwargs=ex_search_kwargs)
 excl_model = RetrievalQA.from_chain_type(llm=excl_chatgpt,
                                          chain_type="stuff",
